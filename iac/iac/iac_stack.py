e1ee8fd116734c9dca87f8677073581f7cea8f13
mateja82 <matejajovanovic@gmail.com>
2020-04-30 19:34:33 +0200

    old Unicorn deleted

--- iac/iac/iac_stack.py
+++ iac/iac/iac_stack.py
@@ -1,5 +1,6 @@
-from aws_cdk import(
-        core,
+from aws_cdk import (
+    core,
+    aws_s3 as s3,
 )
 
 class IacStack(core.Stack):
@@ -7,12 +8,10 @@ class IacStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # Create S3 Bucket, a new object of class Bucket
-        #bucket = s3.Bucket(self, "S3BucketUnicorn",
-        #                bucket_name="S3BucketUnicorn",
-        #                access_control=s3.BucketAccessControl.PUBLIC_READ,
-        #                )
-        # Since we'll be using this bucket to store publically available images, we can give Public Access.
-        #bucket.grant_public_access()
+        # The code that defines your stack goes here
+        bucket = s3.Bucket(self, "s3bucket-unicorn",
+                        bucket_name="s3bucket-unicorn",
+                        access_control=s3.BucketAccessControl.PUBLIC_READ,
+                        )
 
-
+        bucket.grant_public_access()
--- iac/setup.py
+++ iac/setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     packages=setuptools.find_packages(where="iac"),
 
     install_requires=[
-        "aws-cdk.core==1.36.0",
+        "aws-cdk.core==1.36.1",
     ],
 
     python_requires=">=3.6",
